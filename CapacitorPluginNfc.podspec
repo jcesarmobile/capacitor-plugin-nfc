19fb7ab724601c9f93fd065885df317c0e81bb4d
Julio César <jcesarmobile@gmail.com>
2018-06-24 21:32:32 +0200

    Initial iOS implementation

--- CapacitorPluginNfc.podspec
+++ CapacitorPluginNfc.podspec
@@ -1,13 +1,13 @@
 
   Pod::Spec.new do |s|
     s.name = 'CapacitorPluginNfc'
-    s.version = '0.0.1'
+    s.version = '0.0.3'
     s.summary = 'A Capacitor NFC plugin'
     s.license = 'MIT'
     s.homepage = 'https://github.com/jcesarmobile/capacitor-plugin-nfc'
     s.author = 'jcesarmobile'
     s.source = { :git => 'https://github.com/jcesarmobile/capacitor-plugin-nfc', :tag => s.version.to_s }
     s.source_files = 'ios/Plugin/Plugin/**/*.{swift,h,m,c,cc,mm,cpp}'
-    s.ios.deployment_target  = '10.0'
+    s.ios.deployment_target  = '11.0'
     s.dependency 'Capacitor'
   end
--- ios/Plugin/Plugin/Plugin.swift
+++ ios/Plugin/Plugin/Plugin.swift
@@ -1,17 +1,37 @@
 import Foundation
 import Capacitor
+import CoreNFC
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(CapacitorNFCPlugin)
-public class CapacitorNFCPlugin: CAPPlugin {
+public class CapacitorNFCPlugin: CAPPlugin, NFCNDEFReaderSessionDelegate {
     
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
+  @objc func begin(_ call: CAPPluginCall) {
+    let session = NFCNDEFReaderSession(delegate: self,
+                                       queue: DispatchQueue(label: "queueName", attributes: .concurrent), invalidateAfterFirstRead: false)
+    session.begin()
+    call.success()
+  }
+  
+  public func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+    print("invalidated")
+    notifyListeners("invalidatedSession", data: [
+      "message": "NFC session was invalidated"
+    ])
+  }
+  
+  public func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+    for message in messages {
+      for record in message.records {
+        notifyListeners("tagDetected", data: [
+          "payload": String(data: record.payload, encoding: .utf8)!,
+          "identifier": String(data: record.identifier, encoding: .utf8)!,
+          "type": String(data: record.type, encoding: .utf8)!
         ])
+      }
     }
+  }
 }
